7d2e064751e3b9dcb70ff2b302796a3b561bcff6
Fosper <fosper@icloud.com>
2020-12-14 08:15:43 +0300

    minor update

--- index.js
+++ index.js
@@ -11,46 +11,6 @@ class Srvlight extends events {
         this.routes = {}
     }
 
-    // static ws = (customOptions = {}) => {
-    //     const options = {
-    //         url: 'any',
-    //         port: 88,
-    //         max_body_size_bytes: 10240,
-    //         request_timeout_ms: 10000,
-    //         response_timeout_ms: 10000,
-    //         available_ips: [],
-    //         unavailable_ips: [],
-    //         available_methods: ['GET','POST','PUT','DELETE']
-    //     }
-
-    //     for (const customOption of Object.keys(customOptions)) {
-    //         options[customOption] = customOptions[customOption]
-    //     }
-
-    //     return new this('ws', options)
-    // }
-
-    // static wss = (customOptions = {}) => {
-    //     const options = {
-    //         url: 'any',
-    //         port: 8433,
-    //         cert: __dirname + '/SSL/cert.pem',
-    //         key: __dirname + '/SSL/key.pem',
-    //         max_body_size_bytes: 10240,
-    //         request_timeout_ms: 10000,
-    //         response_timeout_ms: 10000,
-    //         available_ips: [],
-    //         unavailable_ips: [],
-    //         available_methods: ['GET','POST','PUT','DELETE']
-    //     }
-
-    //     for (const customOption of Object.keys(customOptions)) {
-    //         options[customOption] = customOptions[customOption]
-    //     }
-
-    //     return new this('wss', options)
-    // }
-
     route = (path, callback, customOptions = {}) => {
         let options = {}
         for (const option of Object.keys(this.options)) {
